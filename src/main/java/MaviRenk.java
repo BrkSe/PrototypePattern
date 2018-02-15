8a8cf42ca72ceefc1a840b90dff70da5db286cfd
Burak <info@burakkutbay.com>
2018-02-15 23:02:37 +0300

    Silindi

--- src/main/java/MaviRenk.java
+++
@@ -1,5 +0,0 @@
-public class MaviRenk implements Renk {
-    public Renk renkOlustur() {
-        return null;
-    }
-}
--- src/main/java/Renk.java
+++
@@ -1,3 +0,0 @@
-public interface Renk extends Cloneable{
-    public Renk renkOlustur();
-}
